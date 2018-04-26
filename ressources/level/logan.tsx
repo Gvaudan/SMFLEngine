16106bcafe5e58a35bc12a945e9d04a20c98c39e
dyoubi <dyoubi@live.fr>
2018-04-27 00:09:15 +0200

    Remove Useless dir

--- ressources/level/logan.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="logan" tilewidth="32" tileheight="32" tilecount="600" columns="20">
- <image source="../bin/ressources/sprites/logan.png" width="665" height="972"/>
-</tileset>
