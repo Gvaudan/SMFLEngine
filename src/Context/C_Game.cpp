0d5b5e8fc42671b948a885f8e37d1f7a7b9baaeb
Gvaudan <dyoubi@live.fr>
2018-04-28 02:07:08 +0200

    Fixing FPS, Fixing frame time

--- src/Context/C_Game.cpp
+++ src/Context/C_Game.cpp
@@ -12,67 +12,83 @@
 using namespace std;
 
 C_Game::C_Game()
-        : m_window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE, WINDOW_STYLE),
-          m_world(m_window), mFont(), mStatisticsText(), mStatisticsUpdateTime(), mStatisticsNumFrames(0) {
-    m_window.setFramerateLimit(60);
+    : m_window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE, WINDOW_STYLE),
+      m_world(m_window), mFont(), mStatisticsText(), mStatisticsUpdateTime(), mStatisticsNumFrames(0) {
+  m_window.setFramerateLimit(60);
 }
 
 void C_Game::run() {
-    cout << __PRETTY_FUNCTION__ << endl;
-    C_Timer::get_instance()->start();
-    sf::Time timeSinceLastUpdate = sf::Time::Zero;
+  cout << __PRETTY_FUNCTION__ << endl;
+  C_Timer::get_instance()->start();
 
-    while (m_window.isOpen()) {
+  sf::Time timeSinceLastUpdate = sf::Time::Zero;
+  sf::Time accumulator = sf::Time::Zero;
+  sf::Time currentTime = C_Timer::get_instance()->get_time();
+  sf::Time frameTime = sf::Time::Zero;
+  sf::Time time = sf::Time::Zero;
 
-        update();
-        handleEvent();
-        update_static(timeSinceLastUpdate);
-        render();
+  const sf::Time dt = sf::seconds(0.1f);
+
+  while (m_window.isOpen()) {
+
+    timeSinceLastUpdate = C_Timer::get_instance()->restart();
+    frameTime = timeSinceLastUpdate - currentTime;
+    currentTime = timeSinceLastUpdate;
+    accumulator += frameTime;
+
+    while (accumulator >= dt) {
+      accumulator -= dt;
+      time += dt;
     }
-    C_Timer::get_instance()->stop();
+
+    update();
+    update_static(timeSinceLastUpdate);
+    handleEvent();
+    render();
+  }
+  C_Timer::get_instance()->stop();
 }
 
 bool C_Game::init() {
-    BOOST_LOG_TRIVIAL(info) << __PRETTY_FUNCTION__;
-    //TextureManager::get_instance()->load("","");
+  BOOST_LOG_TRIVIAL(info) << __PRETTY_FUNCTION__;
 
-    m_font = C_RessourcesManager::get_instance()->load_font("FPS", "ressources/fonts/8-Bit Madness.ttf");
-    mFont = *m_font.get();
-    mStatisticsText.setFont(mFont);
-    mStatisticsText.setCharacterSize(24); // in pixels, not points!
-    mStatisticsText.setFillColor(sf::Color::Red);
+  m_font = C_RessourcesManager::get_instance()->load_font("FPS", "ressources/fonts/8-Bit Madness.ttf");
+  mFont = *m_font.get();
+  mStatisticsText.setFont(mFont);
+  mStatisticsText.setCharacterSize(24); // in pixels, not points!
+  mStatisticsText.setFillColor(sf::Color::Red);
 
-    C_EntityFactory::get_instance()->RegisterType("test-sprite", new C_TestEntityCreator());
-    m_test_entitie = (C_TestEntitie *) C_EntityFactory::get_instance()->Create("test-sprite");
-    m_test_entitie->set_current_state(new C_TestEntitieState(m_test_entitie));
+  C_EntityFactory::get_instance()->RegisterType("test-sprite", new C_TestEntityCreator());
+  m_test_entitie = (C_TestEntitie *) C_EntityFactory::get_instance()->Create("test-sprite");
+  m_test_entitie->set_current_state(new C_TestEntitieState(m_test_entitie));
 
-    C_TestStateMachine::get_instance()->set_entitie(m_test_entitie);
-    C_TestStateMachine::get_instance()->push_state(m_test_entitie->get_current_state());
+  C_TestStateMachine::get_instance()->set_entitie(m_test_entitie);
+  C_TestStateMachine::get_instance()->push_state(m_test_entitie->get_current_state());
 
-    C_GameStateMachine::get_instance()->push_state(new C_InGameState());
+  C_GameStateMachine::get_instance()->push_state(new C_InGameState());
 
-    BOOST_LOG_TRIVIAL(trace) << __PRETTY_FUNCTION__ << " : FPS : " << m_fps.asMicroseconds();
+  BOOST_LOG_TRIVIAL(trace) << __PRETTY_FUNCTION__ << " : FPS : " << m_fps.asMicroseconds();
 
-    return false;
+  return false;
 }
 
 void C_Game::update() {
-    m_test_entitie->update(C_Timer::get_instance()->get_time());
-    C_GameStateMachine::get_instance()->update();
+  m_test_entitie->update(C_Timer::get_instance()->get_time());
+  C_GameStateMachine::get_instance()->update();
 }
 
 void C_Game::render() {
-    m_window.clear();
+  m_window.clear();
 
-    m_window.setView(m_window.getDefaultView());
-    m_test_entitie->draw(m_window, sf::RenderStates::Default);
-    m_window.draw(mStatisticsText);
-    m_window.display();
+  m_window.setView(m_window.getDefaultView());
+  m_test_entitie->draw(m_window, sf::RenderStates::Default);
+  m_window.draw(mStatisticsText);
+  m_window.display();
 }
 
 void C_Game::handleEvent() {
-    sf::Event event;
-    C_EventHandler::get_instance()->process_event(event, &m_window);
+  sf::Event event;
+  C_EventHandler::get_instance()->process_event(event, &m_window);
 }
 
 void C_Game::exit() {
@@ -84,16 +100,16 @@ void C_Game::clean() {
 }
 
 void C_Game::update_static(sf::Time p_elapsed_time) {
-    mStatisticsUpdateTime += p_elapsed_time;
-    mStatisticsNumFrames += 1;
+  mStatisticsUpdateTime += p_elapsed_time;
+  mStatisticsNumFrames += 1;
 
-    if (mStatisticsUpdateTime >= sf::seconds(1.0f)) {
-        std::string str = "FPS = " + std::to_string(mStatisticsNumFrames) + "\n" +
-                          "TimeUpdate = " +
-                          std::to_string(mStatisticsUpdateTime.asMicroseconds() / mStatisticsNumFrames) +
-                          "us";
-        mStatisticsText.setString(str);
-        mStatisticsUpdateTime -= sf::seconds(1.0f);
-        mStatisticsNumFrames = 0;
-    }
+  if (mStatisticsUpdateTime >= sf::seconds(1.0f)) {
+    std::string str = "FPS = " + std::to_string(mStatisticsNumFrames) + "\n" +
+                      "TimeUpdate = " +
+                      std::to_string(mStatisticsUpdateTime.asMilliseconds() / mStatisticsNumFrames) +
+                      "ms";
+    mStatisticsText.setString(str);
+    mStatisticsUpdateTime -= sf::seconds(1.0f);
+    mStatisticsNumFrames = 0;
+  }
 }
